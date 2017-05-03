2dfd0c403d27ba5df3d8e844399ae8466f3821a3
Seungwon Park <psw14041@gmail.com>
2017-05-03 12:46:45 +0900

    Changed spaces to TAB

--- OceanColor/DataOrder/OC_crawler_single_threaded.py
+++ OceanColor/DataOrder/OC_crawler_single_threaded.py
@@ -8,21 +8,20 @@ urls = []
 read_splited = raw_lists.split('<')
 print(read_splited)
 for i in range(1,len(read_splited)):
-    urls.append(read_splited[i].split('>')[0])
+	urls.append(read_splited[i].split('>')[0])
 
 	
 k = 0
 while(k < len(urls)):
-    try: # This enables us to try downloading again if temporary network error occurs.
-        temp = urls[k].split('/')
-        filename = urls[k].split('/')[len(temp)-1]
-        print ('Downloading ' + filename + '...')
-        # conn.request('GET', urls[k], '', headers)
-        # resp = conn.getresponse()
-        # image = resp.read()
-        # f = open('Downloads/' + filename, 'wb')
-        # f.write(image)
-        testfile.retrieve(urls[k], 'Downloads/' + filename)
-        k += 1
-    except: # TODO : I can't remember the error name... Can anyone tell me?
-        print('Temporary downloading error.')
+	try: # This enables us to try downloading again if temporary network error occurs.
+		filename = urls[k].split('/')[len(temp)-1]
+		print ('Downloading ' + filename + '...')
+		# conn.request('GET', urls[k], '', headers)
+		# resp = conn.getresponse()
+		# image = resp.read()
+		# f = open('Downloads/' + filename, 'wb')
+		# f.write(image)
+		testfile.retrieve(urls[k], 'Downloads/' + filename)
+		k += 1
+	except: # TODO : I can't remember the error name... Can anyone tell me?
+		print('Temporary downloading error.')
