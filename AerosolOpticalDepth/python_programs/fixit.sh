e1c1c6dc1a79e63e61b600ca823d4ee2374dd505
Kiehyun Kevin Park <guitar79@naver.com>
2017-06-08 18:07:37 +0930

    Add files via upload

---
+++ AerosolOpticalDepth/python_programs/fixit.sh
@@ -0,0 +1,10 @@
+conda install matplotlib==1.5
+conda install netcdf4
+conda install pyhdf
+conda install -c cistools pyhdf=0.9.0
+conda install basemap
+pip install pyresample
+conda install scipy
+
+
+
