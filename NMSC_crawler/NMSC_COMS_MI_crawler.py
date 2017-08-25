f0190493b1a514ed9f113494b2e30a6269d8f237
Kiehyun Kevin Park <guitar79@naver.com>
2017-08-25 21:57:57 +0900

    yyyyy

--- NMSC_crawler/NMSC_COMS_MI_crawler.py
+++ NMSC_crawler/NMSC_COMS_MI_crawler.py
@@ -9,12 +9,13 @@ for year in range(2017,2018):
 		for Da in range(1,32):
 			for Ho in range(24):
 				for Mn in range(0,60,15): # looks like the interval is 15min. often omitted.
-					url = "http://nmsc.kma.go.kr/emcoms/BIMG/COMS/Y%d/M08/D24/coms_mi_le1b_ir1_a_%d%02d%02d%02d%02d.png" \
+					#url = "http://nmsc.kma.go.kr/emcoms/BIMG/COMS/Y%d/M%02d/D%02d/coms_mi_le1b_ir1_a_%d%02d%02d%02d%02d.png" \
+					url = "http://nmsc.kma.go.kr/emcoms/BIMG/COMS/Y%d/M%02d/D%02d/coms_mi_le1b_ir1_a_%d%02d%02d%02d%02d.png" \
 					% (year, Mo, Da, year, Mo, Da, Ho, Mn)
 					filename = url.split('/')[-1]
 					try:
-						urllib.request.urlretrieve(url, 'Downloads/%s' % filename)
 						print ('Trying %s' % url)
+						urllib.request.urlretrieve(url, 'Downloads/%s' % filename)
 						print ('Downloaded %s' % filename)
 						
 					except urllib.error.HTTPError: # image file doens't exists
