261d712f901703ba41a2489f4ababa457e33bb04
Kiehyun Kevin Park <guitar79@naver.com>
2017-08-26 14:05:12 +0900

    Add files via upload

--- NMSC_crawler/NMSC_COMS_MI_crawler.py
+++ NMSC_crawler/NMSC_COMS_MI_crawler.py
@@ -5,7 +5,7 @@
 #http://nmsc.kma.go.kr/emcoms/BIMG/COMS/Y2017/M08/D26/coms_mi_le1b_ir1_cf_201708260215.png # fulldisk asia
 """
 #chl = [vis, ir1, ir2, swir, wv]
-chl = "ir1"
+chl = "ir2"
 #area = [cf, cn, a, k, lk]
 area = "a"
 import urllib.request
@@ -15,16 +15,16 @@ for year in range(2017,2018):
 		for Da in range(1,32):
 			for Ho in range(24):
 				for Mn in range(0,60,5): # looks like the interval is 5min. often omitted.
-					url = "http://nmsc.kma.go.kr/emcoms/BIMG/COMS/Y%d/M%02d/D%02d/coms_mi_le1b_%d_%d_%d%02d%02d%02d%02d.png" \
+					url = "http://nmsc.kma.go.kr/emcoms/BIMG/COMS/Y%d/M%02d/D%02d/coms_mi_le1b_%s_%s_%d%02d%02d%02d%02d.png" \
 					% (year, Mo, Da, chl, area, year, Mo, Da, Ho, Mn)
 					filename = url.split('/')[-1]
 					my_file = Path('Downloads/%s' % filename)
 					if my_file.is_file(): # image file already exists in my folder
-					    print ('File exists')
+					    print ('File exists%s' % filename)
 					else:
 					    try:
 					        print ('Trying %s' % url)
 					        urllib.request.urlretrieve(url, 'Downloads/%s' % filename)
 					        print ('Downloaded %s' % filename)
-					    except  FileNotFoundError: # image file doesn't exists in the server
-					        print ('file already exist %s' % filename)
+					    except  urllib.error.HTTPError: # image file doesn't exists in the server
+					        print ('Download error %s' % filename)
