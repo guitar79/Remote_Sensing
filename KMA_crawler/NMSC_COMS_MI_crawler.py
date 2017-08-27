1539f580d5730808f2c2fe32652ea409b28dd54b
Kiehyun Kevin Park <guitar79@naver.com>
2017-08-27 09:01:02 +0900

    Add files via upload

--- KMA_crawler/NMSC_COMS_MI_crawler.py
+++ KMA_crawler/NMSC_COMS_MI_crawler.py
@@ -5,16 +5,16 @@
 #http://nmsc.kma.go.kr/emcoms/BIMG/COMS/Y2017/M08/D26/coms_mi_le1b_ir1_cf_201708260215.png # fulldisk asia
 """
 #chl = [vis, ir1, ir2, swir, wv]
-chl = "swir"
+chl = "ir1"
 #area = [cf, cn, a, k, lk]
 area = "a"
 import urllib.request
 from pathlib import Path
-for year in range(2016,2017):
-	for Mo in range(1,13):
+for year in range(2014,2016):
+	for Mo in range(12,13):
 		for Da in range(1,32):
 			for Ho in range(24):
-				for Mn in range(0,60,5): # looks like the interval is 5min. often omitted.
+				for Mn in range(0,60,15): # looks like the interval is 5min. often omitted.
 					url = "http://nmsc.kma.go.kr/emcoms/BIMG/COMS/Y%d/M%02d/D%02d/coms_mi_le1b_%s_%s_%d%02d%02d%02d%02d.png" \
 					% (year, Mo, Da, chl, area, year, Mo, Da, Ho, Mn)
 					filename = url.split('/')[-1]
@@ -23,9 +23,8 @@ for year in range(2016,2017):
 					    print ('File exists%s' % filename)
 					else:
 					    try:
-					        print ('Trying %s' % url)
+					        print ('Trying %s' % filename)
 					        urllib.request.urlretrieve(url, 'Downloads/%s' % filename)
-					        print ('Downloaded %s' % filename)
+					        print ('Downloaded %s' % url)
 					    except  urllib.error.HTTPError: # image file doesn't exists in the server
-					        print ('Download error %s' % filename)
-
+					        print ('Error %s' % filename)
