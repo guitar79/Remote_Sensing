aff9c09f276b9557e57976111c91f8fa49d9851e
Kiehyun Kevin Park <guitar79@naver.com>
2017-08-27 09:01:32 +0900

    Add files via upload

--- KMA_crawler/KMA_AWS_crawler_multithreading.py
+++ KMA_crawler/KMA_AWS_crawler_multithreading.py
@@ -12,7 +12,7 @@ import threading
 
 import sys
 
-prefix = 'output'
+prefix = 'AWS-1min'
 
 #single thread class
 class crawler():
@@ -30,12 +30,11 @@ class crawler():
 				url = "http://www.kma.go.kr/cgi-bin/aws/nph-aws_txt_min?%d%02d%02d%02d%02d&0&MINDB_01M&0&a" % (self.year, self.month, self.day, self.hour, self.minute)
 				soup = BeautifulSoup(urlopen(url), "html.parser")
 				mytable = soup.find_all('table')
-				mytable = soup.find_all('table')
+				#mytable = soup.find_all('table')
 				#only mytable[1] contains weather data
 				for trs in mytable[1].find_all('tr'):
 					for tds in trs.find_all('td'):
 						#print data
-						#output += "%d%02d%02d%02d%02d" % (year, Mo, Da, Ho, Mn)
 						self.output += tds.text
 						#csv delimeter
 						self.output += ','
@@ -51,25 +50,26 @@ class crawler():
 
 #crawler for single month
 class crawler_month(threading.Thread):
-	def __init__(self, year, month, threadno):
+	def __init__(self, year, month, day, threadno):
 		threading.Thread.__init__(self)
 		self.year = year
 		self.month = month
+		self.day = day
 		self.threadno = threadno
 		sys.stderr.write('Thread #%d started...\n' % (self.threadno))
 
 	def run(self):
-		for Da in range(1,32):
-			for Ho in range(0,24):
-				for Mn in range(0,60): 
-					fetcher = crawler(self.year, self.month, Da, Ho, Mn)
-					fetcher.fetch()
-					sys.stderr.write('Thread #%d - fetched %d-%02d-%02d %02d:%02d...\n' % (self.threadno, self.year, self.month, Da, Ho, Mn))
+		for Ho in range(0,24):
+			for Mn in range(0,60): 
+				fetcher = crawler(self.year, self.month, self.day, Ho, Mn)
+				fetcher.fetch()
+				sys.stderr.write('Thread #%d - fetched %d-%02d-%02d %02d:%02d...\n' % (self.threadno, self.year, self.month, self.day, Ho, Mn))
 
 
 threadno = 0
-for year in range(2015,2016):
+for year in range(2016,2017):
 	for Mo in range(1,2):
-		cmonth = crawler_month(year, Mo, threadno)
-		cmonth.start()
-		threadno += 1
+		for Da in range(1,32):
+			cmonth = crawler_month(year, Mo, Da, threadno)
+			cmonth.start()
+threadno += 1
